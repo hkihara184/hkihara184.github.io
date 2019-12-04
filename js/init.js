2494785ca4671ccc85bfe416b3dbf6e1a0592474
Hitoshi Kihara <h.kihara@ntw.co.jp>
2019-12-04 20:40:51 +0900

    fixed issue #1,2,4,6,7,8

--- js/init.js
+++ js/init.js
@@ -78,6 +78,15 @@
 				hoverDelay: 0
 			});
 
+        $('a[href^="#"]').click(function() {
+          var speed = 400;
+          var href= $(this).attr("href");
+          var target = $(href == "#" || href == "" ? 'html' : href);
+          var position = target.offset().top;
+          $('body,html').animate({scrollTop:position}, speed, 'swing');
+          return false;
+        });
+
 	});
 
 })(jQuery);
